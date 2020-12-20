b87817c448233d6e7f7e13765a45d8521eeb314c
Katsuyoshi Ito <kito@itosoft.com>
2020-12-21 00:35:49 +0900

    show irep for debug

--- lib/mrubyplc/cli.rb
+++ lib/mrubyplc/cli.rb
@@ -5,9 +5,14 @@ include FileUtils
 
 module MRubyPlc
   class CLI < Thor
+
+    default_command :build
+
     desc "build", "Build a project"
     def build(path)
       p path
+      irep = load path
+      p irep
     end
   end
 end
